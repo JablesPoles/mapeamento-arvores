6389a4cd90a59eb97da089e8814401bea5fc506d
JablesPoles <matheuspolesnunes@gmail.com>
2025-05-14 16:05:56 -0300

    --allow-empty-message

--- index.php
+++ index.php
@@ -96,8 +96,7 @@
     #theme-toggle-button svg {
         transition: opacity 0.3s ease, transform 0.3s ease;
     }
-  </script>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+  </style>
 </head>
 <body class="bg-light-bg dark:bg-dark-bg text-gray-800 dark:text-dark-text">
 
@@ -313,6 +312,6 @@
         console.warn('Swiper não está definido ou o container .mySwiper não foi encontrado.');
       }
     });
-</script>
+  </script>
 </body>
 </html>
